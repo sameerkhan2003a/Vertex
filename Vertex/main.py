eac2f9d2ad73e9ac503b053de305bb2ad6c53f12
sameerkhan2003a <153911733+sameerkhan2003a@users.noreply.github.com>
2023-12-18 00:15:19 +0530

    Update main.py

--- Vertex/main.py
+++ Vertex/main.py
@@ -4,30 +4,15 @@ import openai
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
 import json
+import os
 
 app = Flask(__name__)
 
-secret = secrets.token_urlsafe(32)
-app.secret_key = secret  # set the secret key
 
-def save_users(users):
-    with open('users2.json', 'w') as f:
-        json.dump(users, f)
 
-def load_users():
-    try:
-        with open('users2.json', 'r') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
+    
 
-users = load_users()
-
-# This function checks if the user is logged in
-def is_logged_in():
-    return 'username' in session
-
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     image_url0 = None
     image_url1 = None
@@ -36,7 +21,8 @@ def index():
         description = request.form['desc']
         style = request.form['style']
 
-        client = openai.OpenAI(api_key='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')
+     
+        client = openai.OpenAI(api_key=os.environ.get('secret_key'))
 
         try:
             response1 = client.images.generate(
@@ -72,64 +58,3 @@ def index():
             print(f"An error occurred: {e}")
 
     return render_template('index.html', url0=image_url0,url1=image_url1,url2=image_url2)
-
-@app.route('/about', methods=['GET', 'POST'])
-def about():
-    return render_template('about.html')
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
-
-@app.route('/SignUp', methods=['GET', 'POST'])
-def SignUp():
-    error = None
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        rePassword = request.form['rePassword']
-
-        # Check if passwords match
-        if password != rePassword:
-            error = 'Passwords do not match! Please try again.'
-        elif username in users:  # Check if username already exists
-            error = 'Username already exists! Please choose a different username.'
-        else:
-            # Store the username and hashed password
-            users[username] = generate_password_hash(password)
-            save_users(users)  # Save users to file
-            flash('Signup successful! You can now log in.')
-            return redirect(url_for('Login'))  # Redirect to login page after successful signup
-
-    return render_template('SignUp.html', error=error)
-
-
-@app.route('/Login', methods=['GET', 'POST'])
-def Login():
-    error = None
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        # Check if username exists and password is correct
-        if username in users and check_password_hash(users[username], password):
-            session['username'] = username  # log the user in by setting a session variable
-            return redirect(url_for('index'))  # Redirect to index page after successful login
-        else:
-            error = 'Invalid username or password! Please try again.'
-    return render_template('Login.html', error=error)
-
-@app.route('/navbar', methods=['GET', 'POST'])
-def navbar():
-    return render_template('navbar.html')
-
-@app.route('/Logout')
-def Logout():
-    session.pop('username', None)  # Clear the 'username' key from the session
-    flash('You have been logged out.')
-    return redirect(url_for('Login'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-    
